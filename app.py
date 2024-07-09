c090c48ebcc4dd4248dc48f59942c92b021e57b5
Nirupama <nirupamasinghab12@gmail.com>
2024-07-09 19:54:30 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Sep 25 16:28:10 2022
-
-@author: maxim migutin
-"""
-
-
 import joblib
 from PIL import Image
 import pandas as pd
 import streamlit as st
 
 # loading pre-trained classifier
-model = joblib.load(open('./classification_model.sav', 'rb'))
+model = joblib.load('classification_model.sav')
 
 # main App layout
 st.header("ML Iris Classificator:")
@@ -20,10 +12,11 @@ image = Image.open('irises.jpeg')
 st.image(image, use_column_width=True)
 st.write("Feed flower properties for automatic classification:")
 
-SepalLengthCm = st.slider('SepalLengthCm:', 2.01, 6.01)
-SepalWidthCm = st.slider('SepalWidthCm:', 0.0, 5.01)
-PetalLengthCm = st.slider('PetalLengthCm',0.0, 3.01)
-PetalWidthCm = st.slider('PetalWidthCm:', 0.0, 2.01)
+# Define slider ranges based on Iris dataset feature ranges
+SepalLengthCm = st.slider('SepalLengthCm:', 2.0, 8.0, step=0.1)
+SepalWidthCm = st.slider('SepalWidthCm:', 2.0, 4.5, step=0.1)
+PetalLengthCm = st.slider('PetalLengthCm:', 1.0, 7.0, step=0.1)
+PetalWidthCm = st.slider('PetalWidthCm:', 0.1, 2.5, step=0.1)
 
 data = {'SepalLengthCm': SepalLengthCm,
         'SepalWidthCm': SepalWidthCm,
@@ -37,8 +30,6 @@ pred_proba = model.predict_proba(features)
 st.subheader('Class probabilities:') 
 
 # Results
-st.write('**Setosa: `{}`%**:'.format(round(pred_proba[0][0]*100, 2)))
+st.write('**Setosa: `{}`%**'.format(round(pred_proba[0][0]*100, 2)))
 st.write('**Versicolor: `{}`%**'.format(round(pred_proba[0][1]*100, 2)))
 st.write('**Virginica: `{}`%**'.format(round(pred_proba[0][2]*100, 2)))
-
-
